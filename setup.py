0b0a9e6c183f94f77351cb0fc1188411a17d7a12
leleist <l.leist@leist-design.de>
2025-03-25 17:16:58 +0100

    version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exactrasterpolyextract',
-    version='0.1.0',
+    version='0.1.1',
     license='Apache 2.0',
     install_requires=[
         'pandas>=2.2.3',
