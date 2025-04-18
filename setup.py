5bf54b86d4c59499098732fe0354057ce242d60b
leleist <l.leist@leist-design.de>
2025-04-18 14:46:23 +0200

    patch increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exactrasterpolyextract',
-    version='0.1.1',
+    version='0.1.2',
     license='Apache 2.0',
     install_requires=[
         'pandas>=2.2.3',
