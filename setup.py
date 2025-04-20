cafb2d0684d5ab6788e4d54fbbee70bfa4108fbc
leleist <l.leist@leist-design.de>
2025-04-20 16:34:43 +0200

    patch increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exact_raster_poly_extract',
-    version='1.0.0',
+    version='1.0.1',
     license='Apache 2.0',
     install_requires=[
         'pandas>=2.2.3',
